7bf9c63b25e48a0ba8ab015b16a36f98a5073464
서영덕_T5105 <50124721+SeoYoungDeok@users.noreply.github.com>
2023-09-05 18:25:47 +0900

    [Silver V] Title: 소트인사이드, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -3,9 +3,5 @@ from collections import defaultdict
 
 array = list(s.readline().strip())
 
-num_dict = defaultdict(int)
-
-for i in array:
-    num_dict[i] += 1
-
-print("".join([str(i) * num_dict[str(i)] for i in range(9, -1, -1)]))
+array.sort(reverse=True)
+print("".join(array))
