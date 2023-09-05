cb95ea37bb24d3d77258287c8cb1cf495b44f256
서영덕_T5105 <50124721+SeoYoungDeok@users.noreply.github.com>
2023-09-05 23:43:02 +0900

    [Silver IV] Title: 수 찾기, Time: 300 ms, Memory: 155268 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,18 @@
+from sys import stdin as s
+from collections import defaultdict
+
+N = list(s.readline().strip())
+array = list(s.readline().strip().split())
+
+num_dict = defaultdict(int)
+for i in array:
+    num_dict[str(i)] = 1
+
+M = list(s.readline().strip())
+array = list(s.readline().strip().split())
+
+for i in array:
+    if num_dict[str(i)]:
+        print(1)
+    else:
+        print(0)
