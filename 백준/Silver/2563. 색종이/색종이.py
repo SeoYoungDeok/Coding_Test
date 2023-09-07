b78e471b34ec87a254268d6e72d481e38e379715
서영덕_T5105 <50124721+SeoYoungDeok@users.noreply.github.com>
2023-09-08 00:40:28 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -2,7 +2,7 @@ from sys import stdin as s
 
 N = int(s.readline().strip())
 
-array = [[0 for i in range(100)] for j in range(100)]
+array = [[0]*100 for i in range(100)]
 
 for i in range(N):
     X, Y = map(int, s.readline().strip().split())
