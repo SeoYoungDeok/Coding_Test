df025a5e26031acd1b2e933e414e4bc478633c2c
서영덕_T5105 <50124721+SeoYoungDeok@users.noreply.github.com>
2023-10-31 21:20:28 +0900

    [Silver IV] Title: 듣보잡, Time: 244 ms, Memory: 137320 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,14 +1,14 @@
 import sys
 
-N, M = map(int, input().split())
+N, M = map(int, sys.stdin.readline().split())
 
 N_list = dict()
 for i in range(N):
-    N_list[input()] = 1
+    N_list[sys.stdin.readline().rstrip()] = 1
 
 answer = list()
 for j in range(M):
-    M_name = input()
+    M_name = sys.stdin.readline().rstrip()
     if M_name in N_list:
         answer.append(M_name)
 
